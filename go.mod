caa49d5e3cb54a307206fc00b924f9f8ebfdb99a
王大昭 <wzkf123@gmail.com>
2023-11-17 16:28:33 +0800

    first
    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wangdzhao/common
+
+go 1.21.3
+
+require golang.org/x/crypto v0.15.0
---
+++ secret/password.go
@@ -0,0 +1,15 @@
+package secret
+
+import "golang.org/x/crypto/bcrypt"
+
+// HashPassword 加密密码
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+// CheckPasswordHash 验证密码
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
